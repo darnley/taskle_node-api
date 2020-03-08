48b0aa1ca924c6ca78e330b0dfb5d0e8a2736afc
Darnley Costa <darnleysilva@gmail.com>
2020-03-08 13:26:24 -0300

    Removida a rota /tasks

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import tasks from './tasks'
 import people from './people'
 import projects from './projects'
 import auth from './auth'
 import authentication from '../middlewares/authentication'
 import teams from './teams'
 import keywords from './keywords'
+import me from './me'
 
 const router = Router()
 
@@ -13,8 +13,8 @@ router.use('/auth', auth)
 
 router.use('/people', authentication.authenticate(), people)
 router.use('/projects', authentication.authenticate(), projects)
-router.use('/tasks', authentication.authenticate(), tasks)
 router.use('/teams', authentication.authenticate(), teams)
 router.use('/keywords', authentication.authenticate(), keywords)
+router.use('/me', authentication.authenticate(), me)
 
 export default router
--- src/routes/tasks/getAll.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-
-const getAll = (req: Request, res: Response): void => {
-  res.json({ ready: true })
-}
-
-export default getAll
