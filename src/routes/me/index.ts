2aaecff5148e0c5848ce48dd321deb48281290c2
Darnley Costa <darnleysilva@gmail.com>
2020-03-31 21:21:45 -0300

    Adicionada a API /me/profile

--- src/routes/me/index.ts
+++ src/routes/me/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import projects from './projects'
 import tasks from './tasks'
+import profile from './profile'
 
 const router = Router()
 
 router.get('/projects', projects)
 router.get('/tasks', tasks)
+router.get('/profile', profile)
 
 export default router
