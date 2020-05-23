3e301ea3c59726e882957f18bec06f280f8e11ae
Darnley Costa <darnleysilva@gmail.com>
2020-05-22 22:10:41 -0300

    Adicionado o serviço de e-mail de boas vindas ao incluir uma pessoa

--- src/routes/people/add.ts
+++ src/routes/people/add.ts
@@ -2,18 +2,22 @@ import { Request, Response } from 'express'
 import User from '../../models/User'
 import argon2 from 'argon2'
 import uuid from 'uuid-random'
+import { sendMailOnPersonCreation } from '../../services/mail/sendMailOnPersonCreation'
 
 const add = async (req: Request, res: Response): Promise<void> => {
   const user = new User(req.body)
 
   user.salt = uuid()
-  user.password = await argon2.hash(user.password, { salt: Buffer.from(user.salt) })
+  // user.password = await argon2.hash(user.password, { salt: Buffer.from(user.salt) })
+  user.password = uuid()
   user.isActive = true
 
   user.save()
     .then((user) => {
       delete user.password
       delete user.salt
+
+      sendMailOnPersonCreation(user)
 
       res
         .status(201)
