dc1f63d6f944a36caafb65159877f0bc15d50d35
Darnley Costa <darnleysilva@gmail.com>
2020-05-31 21:07:34 -0300

    Alteração para não trazer minhas tarefas de projetos encerrados

--- src/routes/me/tasks.ts
+++ src/routes/me/tasks.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from 'express'
 import { IUser } from '../../models/User'
 import Task from '../../models/Task'
+import ProjectStatus from '../../enums/projectStatus'
+import { IProject } from '../../models/Project'
 
 const getTasks = (req: Request, res: Response) => {
   const userId = (req.user as IUser)._id
 
   Task
     .find({ responsible: userId })
-    .populate('project', 'name description keywords')
+    .populate('project', 'name description keywords status')
     .populate('responsible', 'firstName lastName')
     .then((tasks) => {
       res
-        .json(tasks)
+        .json(tasks.filter(t => (t.project as IProject).status !== ProjectStatus.Ended))
     })
     .catch((reason) => {
       res
