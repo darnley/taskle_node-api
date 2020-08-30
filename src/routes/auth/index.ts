734584569bb67d0de1f49941eedbe26e9762cc3c
Darnley Costa <darnleysilva@gmail.com>
2020-08-30 20:13:07 -0300

    Adiciona rota para resetar a senha

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import getToken from './getToken'
 import authentication from '../../middlewares/authentication'
 import verifyToken from './verifyToken'
+import passwordReset from './passwordReset'
 
 const router = Router()
 
+router.post('/password-reset/:userId/:key1/:key2', passwordReset)
 router.post('/token', getToken)
 router.post('/verify', authentication.authenticate(), verifyToken)
 
---
+++ src/routes/auth/passwordReset.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express'
+import PasswordReset from '../../models/PasswordReset'
+import User from '../../models/User'
+import uuid from 'uuid-random'
+import argon2 from 'argon2'
+
+const passwordReset = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const userId: string = req.params.userId
+  const key1: string = req.params.key1
+  const key2: string = req.params.key2
+
+  if (!userId || !key1 || !key2) {
+    next(new Error('The request is invalid. All parameters must be sent.'))
+  }
+
+  const passwordResetRequest = await PasswordReset
+    .findOne({
+      user: userId,
+      key1: key1,
+      key2: key2
+    })
+
+  if (!passwordResetRequest) {
+    next(new Error('The request is invalid. We did not found the password reset as requested.'))
+  }
+
+  const salt: string = uuid()
+  const hashedPassword: string = await argon2.hash(req.body.password, { salt: Buffer.from(salt) })
+
+  await User
+    .findByIdAndUpdate(userId, { salt: salt, password: hashedPassword })
+
+  res
+    .status(200)
+    .json({})
+}
+
+export default passwordReset
